2992426f9f8f5d549ec0279583cb97275dcc8f20
semyonbelyaeff <belyaev@uspu.su>
2021-09-12 12:52:34 +0500

    Рабочаяя программа

--- gitdemo/main.cpp
+++ gitdemo/main.cpp
@@ -1,47 +1,60 @@
 /*
  * main.cpp
  *
- *  Created on: 11 ����. 2021 �.
- *      Author: semyo
+ *  Created on: 12 sept. 2021.
+ *      Author: semyon_belyaeff
  */
 #include<iostream>
 #include<limits>
+#include<random>
+
 int main()
 {
-	int choise;
-	do{
-	menu_retry:
-	std::cout<<"         МАГАЗИН\n"
-			   "        ------------\n"
-			   "        1.Яблоки\n"
-			   "        2.Груши\n"
-			   "        3.Апельсины\n"
-			   "        0.Выход\n";
+	std::cout << "Игра \"Камни\". Правила: 2 игрока - пользователь и компьютер - делают ходы по очереди. \nЕсть куча, в ней N камней (N = 10..20 - выбирается случайным образом)\nЗа ход игрок обязан взять 1, 2 или 3 камня.\nКто забрал последний камень - проиграл." << std::endl;
+	std::default_random_engine rnd;
+		std::random_device rdev;
+		rnd.seed(rdev());
+		std::uniform_int_distribution<> stones {10,20};
+		std::uniform_int_distribution<> stnes {1,3};
+ retry:
+		int N=stones(rnd);
+		 while (N > 0)
+		    {
+		        int x;
+		        int m=3;
+
+		        std::cout <<"В куче "<< N << " камней" << std::endl;
+		        do {
+		        	menu_retry:
+		            std::cout << "Ваш ход: " << std::endl;
+		            if (not(std::cin>>x)){std::cout<<"Неккоректный ввод\nТребуется ввести число"<<std::endl;
+		            	std::cin.clear();
+		            	std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+		            	goto menu_retry;}
+		            } while (x <=0 || x>m || x>N);
+		        N -= x;
+		        std::cout << N << " камней в куче" << std::endl;
+		        if (N == 0) {
+		            std::cout << "Вы проиграли" << std::endl;
+		        continue;
+		        }
+		        std::cout << "Мой ход:" << std::endl;
+		        x = stnes(rnd) % m + 1;
+		        if (x > N) x = N;
+		        N -= x;
+		        std::cout << N << " камней в куче" << std::endl;
+		        if (N == 0) {
+		        	std::cout << "Компьютер проиграл" << std::endl;
+		        	 continue;
+		        }}
+
+     	std::cout << "Играем снова? 1 - да, 0 - нет" << std::endl;
+     	int c;
+     	std::cin>>c;
+		if (c==1) goto retry;
+     	if (c==0) std::cout << "До свидания!" << std::endl;
 
 
-	if (not(std::cin>>choise)){std::cout<<"Неккоректный ввод\nТребуется ввести число"<<std::endl;
-	std::cin.clear();
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-	goto menu_retry;
-	}
-	switch(choise){
-	case 0:
-		std::cout<<"До свидания!"<<std::endl;
-		break;
-	case 1:
-		std::cout<<"Заберите ваши яблоки"<<std::endl;
-		break;
-	case 2:
-		std::cout<<"Заберите ваши груши"<<std::endl;
-		break;
-	case 3:
-		std::cout<<"Заберите ваши апельсины"<<std::endl;
-		break;
-	default:
-		std::cout<<"Такого пункта нет"<<std::endl;
-
-	}
-	}while (choise!=0);
 	return 0;
 }
 
